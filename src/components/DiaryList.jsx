7fcf9d39ec9aece507005a3936b734053e7284ce
forhye <4_hye@naver.com>
2024-04-25 17:57:24 +0900

    feat: 정렬 기능 추가

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -2,22 +2,54 @@ import "./DiaryList.css";
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ data }) => {
-    return (
-        <div className="DiaryList">
-            <div className="menu_bar">
-                <select>
-                    <option value={"Latest"}>최신순</option>
-                    <option value={"oldest"}>오래된 순</option>
-                </select>
-                <Button text={"새 일기 쓰기"} type={"POSITIVE"} />
-            </div>
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-            <div className="list_wrapper">
-                {data.map((item) => <DiaryItem key={item.id} {...item} />)}
-            </div>
-        </div>
-    )
-}
+const DiaryList = ({ data }) => {
+  const nav = useNavigate();
+
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedData = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+  // 원본 배열은 그대로 냅두고 정렬된 새로운 배열을 반환한다 toSorted()
+  // js 정렬함수들은 사전순으로 값을 비교하기 때문에 객체로 받은 data의 값을 잘 이해하지 못한다.
+  // 그래서 직접 비교함수를 넣어줘야한다
+
+  const sortedData = getSortedData();
+
+  return (
+    <div className="DiaryList">
+      <div className="menu_bar">
+        <select onChange={onChangeSortType}>
+          <option value={"latest"}>최신순</option>
+          <option value={"oldest"}>오래된 순</option>
+        </select>
+        <Button
+          onClick={() => nav("new")}
+          text={"새 일기 쓰기"}
+          type={"POSITIVE"}
+        />
+      </div>
+
+      <div className="list_wrapper">
+        {sortedData.map((item) => (
+          <DiaryItem key={item.id} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default DiaryList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,58 +6,54 @@ import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 
 const getMonthlyData = (pivotDate, data) => {
-    const beginTime = new Date(
-        pivotDate.getFullYear(),
-        pivotDate.getMonth(),
-        1,
-        0,
-        0,
-        0
-    ).getTime();
+  const beginTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1,
+    0,
+    0,
+    0
+  ).getTime();
 
-    const endTime = new Date(
-        pivotDate.getFullYear(),
-        pivotDate.getMonth() + 1,
-        0,
-        23, 59, 59
-    ).getTime();
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1, // 이번달에 +1을 해주면 다음달이 되고, 다음달의 0일은 이번달의 마지막날이다.
+    0,
+    23,
+    59,
+    59
+  ).getTime();
 
-    return data.filter((item) =>
-        beginTime <= item.createdDate && item.createdDate <= endTime);
-}
+  return data.filter(
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime
+  );
+};
 
 const Home = () => {
+  const data = useContext(DiaryStateContext);
 
-    const data = useContext(DiaryStateContext);
-    const [pivotDate, setPivotDate] = useState(new Date());
+  const [pivotDate, setPivotDate] = useState(new Date());
 
-    const monthlyData = getMonthlyData(pivotDate, data);
-    console.log(monthlyData)
+  const monthlyData = getMonthlyData(pivotDate, data);
 
-    const onIncreaseMonth = () => {
-        setPivotDate(
-            new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1)
-        );
-    };
-    const onDecreaseMonth = () => {
-        setPivotDate(
-            new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1)
-        )
-    };
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
 
-    return (
-        <div>
-            <Header
-                title={
-                    `${pivotDate.getFullYear()}년
-                    ${pivotDate.getMonth() + 1}월`
-                }
-                leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
-                rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
-            />
-            <DiaryList data={monthlyData} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header
+        title={`${pivotDate.getFullYear()}년
+                    ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
+      />
+      <DiaryList data={monthlyData} />
+    </div>
+  );
+};
 
 export default Home;
